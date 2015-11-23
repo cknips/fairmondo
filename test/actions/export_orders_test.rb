d88936cd06317f76adccfb1088e58b1d24ded85a
Marc Altmann <mail@marcaltmann.com>
2015-11-24 00:02:38 +0100

    Work in progress ExportOrders class

--- test/actions/export_orders_test.rb
+++ test/actions/export_orders_test.rb
@@ -21,13 +21,26 @@ describe ExportOrders do
     end
 
     it 'should export order data for business transaction' do
+      lig = FactoryGirl.create(:line_item_group, :sold, message: 'Please handle with extra care!',
+                               purchase_id: 'F00000012')
+
+
+      article = FactoryGirl.create(:article, title: 'Great Expectations', price_cents: 2000)
+
+
       business_transaction = FactoryGirl.create(:business_transaction, seller: user)
 
       export_orders = ExportOrders.new(user, time_range)
 
+      'Purchase ID; Article Title; Quantity; Sold at; Message;
+      Payment Address Title; Payment Address Name; Payment Address Company Name; Payment Address Line 1; Payment Address Line 2; Payment Address Zip; Payment Address City;
+      Shipping Address Title; Shipping Address Name; Shipping Address Company Name; Shipping Address Line 1; Shipping Address Line 2; Shipping Address Zip; Shipping Address City;'
+
       assert_equal("Transaction ID;Article ID;Title;Quantity\n#{business_transaction.id};#{business_transaction.article_id};#{business_transaction.article.title};#{business_transaction.quantity_bought}\n",
                    export_orders.csv_data)
     end
+
+    # order by line item group
 
     it 'should generate a filename containing the dates' do
       date_range = Date.parse('2015-01-01')..Date.parse('2015-10-15')
